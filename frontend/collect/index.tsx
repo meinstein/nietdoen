03f833651829198462b3d23e3da5b520c3497bc5
max <maximillianeinstein@gmail.com>
2024-06-04 11:13:19 +0200

    initial commit

---
+++ frontend/collect/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+
+  const rootNode = document.getElementById('root')
+  const root = createRoot(rootNode as HTMLElement)
+
+  root.render(<div>collect</div>)
