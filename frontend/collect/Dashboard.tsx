256ff01f6bcf5823d94ee5e40cbec99404d08c19
max <maximillianeinstein@gmail.com>
2024-06-18 13:57:11 +0200

    update

--- frontend/collect/Dashboard.tsx
+++ frontend/collect/Dashboard.tsx
@@ -1,24 +1,38 @@
-import { Button, FileInput, Image, Stack, TagsInput, Text, TextInput, Title } from '@mantine/core'
+import {
+  Button,
+  FileInput,
+  Group,
+  Image,
+  Radio,
+  Stack,
+  TagsInput,
+  Text,
+  TextInput,
+  Title,
+} from '@mantine/core'
 import { useGeolocation } from '@uidotdev/usehooks'
+import { addDoc, collection } from 'firebase/firestore'
 import { ref, uploadBytes } from 'firebase/storage'
 import React, { useEffect, useState } from 'react'
-import { model, storage } from './Auth'
+import { db, model, storage } from './Auth'
 
 type AIResponse = {
-  colors: string[]
-  materials: string[]
-  shapes: string[]
-  icons: string[]
-  location: string[]
-  text: string[]
   fonts: string[]
+  colors: string[]
+  text_original: string[]
+  text_english_translation: string[]
+  sign_location: string[]
 }
+
+type AIResponseKeys = keyof AIResponse
 
 export const Dashboard = () => {
   const [error, setError] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
+  const [language, setLanguage] = useState<string>('dutch')
   const [res, setRes] = useState<AIResponse | null>(null)
   const [file, setFile] = useState<File | null>(null)
+  const [storageUrl, setStorageUrl] = useState<string | null>(null)
   const state = useGeolocation({
     enableHighAccuracy: true,
     timeout: 5000,
@@ -28,10 +42,11 @@ export const Dashboard = () => {
   useEffect(() => {
     if (!file) {
       setRes(null)
+      setStorageUrl(null)
     }
   }, [file])
 
-  const onClick = async () => {
+  const onAnalyze = async () => {
     if (!file) return
 
     setError(null)
@@ -45,21 +60,19 @@ export const Dashboard = () => {
       })
       const mimeType = uploadResult.metadata.contentType
       const storageUrl = uploadResult.ref.toString()
+      setStorageUrl(storageUrl)
+
+      const response_fields: AIResponseKeys[] = [
+        'fonts',
+        'colors',
+        'text_original',
+        'text_english_translation',
+        'sign_location',
+      ]
 
       const prompt = JSON.stringify({
-        prompt:
-          'This is a picture of a sign. Describe it. Respond to the following fields. Each field should be an array of values.',
-        response_fields: [
-          'colors',
-          'materials',
-          'shapes',
-          'icons',
-          'location',
-          'text',
-          'fonts',
-          'language',
-          'english_translation',
-        ],
+        instructions: `This is a picture of a sign written in ${language} that forbids bicycles from being parked here. Supply values for the following fields. All values must be arrays of strings.`,
+        response_fields,
       })
 
       if (!mimeType) throw new Error('No MIME type')
@@ -74,12 +87,31 @@ export const Dashboard = () => {
       // To generate text output, call generateContent with the text input
       const { response } = await model.generateContent([prompt, imagePart])
 
-      console.log(JSON.parse(response.text()))
-
       setRes(JSON.parse(response.text()))
     } catch (error) {
       setError(error.message)
     }
+    setLoading(false)
+  }
+
+  const onSave = async () => {
+    if (!res) return
+    setLoading(true)
+
+    const data = {
+      storageUrl,
+      colors: res.colors,
+      fonts: res.fonts,
+      location: res.sign_location,
+      text: res.text_original,
+      translation: res.text_english_translation,
+      lat: state.latitude,
+      lon: state.longitude,
+    }
+
+    await addDoc(collection(db, 'signs'), data)
+    setRes(null)
+    setFile(null)
     setLoading(false)
   }
 
@@ -91,9 +123,15 @@ export const Dashboard = () => {
         placeholder='Tap here to upload image'
         value={file}
         onChange={setFile}
-        capture='environment'
         accept='image/*'
       />
+      <Radio.Group name='language' label='Language' value={language} onChange={setLanguage}>
+        <Group mt='xs'>
+          <Radio value='dutch' label='Dutch' />
+          <Radio value='english' label='English' />
+        </Group>
+      </Radio.Group>
+      <TextInput disabled label='Coordinates' value={`${state.latitude}, ${state.longitude}`} />
       {file && (
         <Image
           mah={200}
@@ -103,15 +141,22 @@ export const Dashboard = () => {
           alt='Uploaded image'
         />
       )}
-      <TextInput disabled label='Coordinates' value={`${state.latitude}, ${state.longitude}`} />
       {res && <TagsInput label='Colors' defaultValue={res.colors} />}
       {res && <TagsInput label='Fonts' defaultValue={res.fonts} />}
-      {res && <TagsInput label='Shapes' defaultValue={res.shapes} />}
-      {res && <TagsInput label='Text' defaultValue={res.text} />}
+      {res && <TagsInput label='Location' defaultValue={res.sign_location} />}
+      {res && <TagsInput label='Text' defaultValue={res.text_original} />}
+      {res && <TagsInput label='English Translation' defaultValue={res.text_english_translation} />}
       {error && <Text c='red'>{error}</Text>}
-      <Button disabled={!file} loading={loading} onClick={onClick}>
-        Generate
-      </Button>
+      {file && !res && (
+        <Button loading={loading} onClick={onAnalyze}>
+          Analyze
+        </Button>
+      )}
+      {file && res && (
+        <Button loading={loading} onClick={onSave}>
+          Save
+        </Button>
+      )}
     </Stack>
   )
 }
